27b845ed7d9b9b89a6e04aa2611560dda2daffff
luanqi77 <623233604@qq.com>
2019-11-11 11:44:45 +0800

    增加了element-ui插件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
 import router from './router'
 import Axios from 'axios'
-import ElementUI from 'element-ui'
+
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$axios=Axios
